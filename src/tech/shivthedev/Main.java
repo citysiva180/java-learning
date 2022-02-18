60a232ceb43248179f355144aaf187a4c7908d7d
citysiva180@gmail.com <citysiva180@gmail.com>
2022-02-18 06:55:07 +0530

    Changes Commit to Git!

---
+++ src/tech/shivthedev/Main.java
@@ -0,0 +1,10 @@
+package tech.shivthedev;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+        //change!
+    }
+}
