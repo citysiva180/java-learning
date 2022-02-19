ce4c633d61ed1ed7782066ea1b1cc6fa62922d04
citysiva180@gmail.com <citysiva180@gmail.com>
2022-02-19 18:28:54 +0530

    Learning Variable Sizes and datatypes

--- src/tech/shivthedev/Main.java
+++ src/tech/shivthedev/Main.java
@@ -4,15 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int age = 30;  //the int is the type while age is an identified | Provisioning value is known as initializing
-        age = 35; //Re-assiging variables
-        //Try declaring variables line by line
-        //You could copy your variables
-        int myAge = 30;
-        int yourAge = 40; //Camel case
-        yourAge = myAge;
+        byte age = 30;
+        int viewCountsInYoutube = 123_456_789;
+        long justinBieberVideos = 3_000_000_000l; //Remember, long values needs l at the end of the number
+        double price = 10.99098098098; //Double gets large decimal value numbers
+        float walmart_price = 10.99f; //Float is for smaller numbers
+        char letter = 'A'; //Remember, always single quote wrapping
+        boolean isEligible = false; //Boolean Values - Yes or No
 
-        System.out.println(yourAge);
+        System.out.println(viewCountsInYoutube);
 
     }
 }
+
