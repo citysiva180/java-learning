00e879dea96fa16394a79d769407ebb5fb4c9a5b
citysiva180@gmail.com <citysiva180@gmail.com>
2022-02-19 19:47:59 +0530

    completed until type conversion and casting

--- src/tech/shivthedev/Main.java
+++ src/tech/shivthedev/Main.java
@@ -4,16 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        byte age = 30;
-        int viewCountsInYoutube = 123_456_789;
-        long justinBieberVideos = 3_000_000_000l; //Remember, long values needs l at the end of the number
-        double price = 10.99098098098; //Double gets large decimal value numbers
-        float walmart_price = 10.99f; //Float is for smaller numbers
-        char letter = 'A'; //Remember, always single quote wrapping
-        boolean isEligible = false; //Boolean Values - Yes or No
+        //Implicit casting ---> Automatic Casting
+        // byte > short > int > long
+        short x = 1;
+        int y = x +2;
 
-        System.out.println(viewCountsInYoutube);
+
+
 
     }
 }
-
