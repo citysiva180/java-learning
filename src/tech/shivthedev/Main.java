6cebd3b2c158330099825bdbab0b877e500a681a
citysiva180@gmail.com <citysiva180@gmail.com>
2022-02-19 18:28:11 +0530

    Learning Variable types

--- src/tech/shivthedev/Main.java
+++ src/tech/shivthedev/Main.java
@@ -3,9 +3,16 @@ package tech.shivthedev;
 public class Main {
 
     public static void main(String[] args) {
-        //Conquer your fears
-        System.out.println("Hello World!, Java is awesome!");
-        //note that your code needs to be chained to make this happen!
-        //Ensure to cover your textual data with double quotes..!
+
+        int age = 30;  //the int is the type while age is an identified | Provisioning value is known as initializing
+        age = 35; //Re-assiging variables
+        //Try declaring variables line by line
+        //You could copy your variables
+        int myAge = 30;
+        int yourAge = 40; //Camel case
+        yourAge = myAge;
+
+        System.out.println(yourAge);
+
     }
 }
