dcfd5c6784ead882f132fc1d718d733ad3ba70ae
citysiva180@gmail.com <citysiva180@gmail.com>
2022-02-19 17:44:43 +0530

    Practicing commit Intelli-j

--- src/tech/shivthedev/Main.java
+++ src/tech/shivthedev/Main.java
@@ -3,12 +3,6 @@ package tech.shivthedev;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World!");
-        //change!
-        //This font suits the coding at its best!!
-        // Fonts changed!!
-        //checking again...
         System.out.println("Learning Java is awesome? Still a questionable thing to think!");
     }
 }
